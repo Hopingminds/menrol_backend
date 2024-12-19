bf8fb9fba022090d6749111718082a36e5163b97
Anshul Karyal <karyalanshul@gmail.com>
2024-12-19 17:36:58 +0530

    sdfg

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -107,7 +107,7 @@ export async function getUser(req, res) {
             return res.status(404).json({ success: false, message: "User not found."});
         }
 
-        return res.status(200).json({ success: false, user });
+        return res.status(200).json({ success: true, user });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ success: false, message: 'Internal Server Error: '+ error.message });
