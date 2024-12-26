415f95bd84f921f85a5d291636ef3d7f8f356356
Anshul Karyal <karyalanshul@gmail.com>
2024-12-26 16:59:46 +0530

    `Update cors.config.js`

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,11 +1,11 @@
 const allowedOrigins = [
-	'*',
 	'http://localhost:3000',
 	'http://localhost:3022',
 	'http://localhost:5173',
 	'https://menrol-admin.vercel.app',
 	'https://www.menrol.com',
-	'https://menrol.com'
+	'https://menrol.com',
+	'*',
 ];
 
 const corsOptions = {
