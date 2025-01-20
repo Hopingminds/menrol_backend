d454a1a356843b332434d3cb1f27ecdc5d52c666
Anshul Karyal <karyalanshul@gmail.com>
2025-01-20 17:24:40 +0530

    SP Auth in initiatePurchaseSubcription

--- src/routes/Payment.routes.js
+++ src/routes/Payment.routes.js
@@ -2,9 +2,10 @@ import { Router } from 'express'
 const router = Router()
 
 import * as PaymentController from '../controllers/PaymentController.js'
+import SPAuth from '../middleware/ServiceProvide.Auth.js';
 
 // POST ROUTES
-router.route('/initiatePurchaseSubcription').post(PaymentController.initiatePurchaseSubcription);
+router.route('/initiatePurchaseSubcription').post(SPAuth, PaymentController.initiatePurchaseSubcription);
 router.route('/initiatePayment').post(PaymentController.initiatePayment);
 
 // GET ROUTES
